c00ad0098023e848c6fbcddaa9b0848a5a229444
Elle Wilson <eleanorwilsonn@gmail.com>
2018-08-17 14:32:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,8 +6,11 @@ function findMatching (list, name) {
   });
 }
 
-function fuzzyMatch(drivers, string) {
-  drivers.filter()
+function fuzzyMatch (list, partialName) {
+  let lengthOfName = partialName.length;
+  return list.filter(function (driverName) {
+    return driverName.slice(0, lengthOfName) === partialName;
+  });
 }
 
 
