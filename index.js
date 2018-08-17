902b537b587eb167ca2c86cee1647339fed6aabc
Elle Wilson <eleanorwilsonn@gmail.com>
2018-08-17 14:31:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // Code your solution in this file
 
-function findMatching(drivers, string) {
-  drivers.filter()
+function findMatching (list, name) {
+  return list.filter(function (driverName) {
+    return driverName.toLowerCase() === name.toLowerCase();
+  });
 }
 
 function fuzzyMatch(drivers, string) {
@@ -12,3 +14,6 @@ function fuzzyMatch(drivers, string) {
 function matchName(drivers, string) {
   drivers.filter()
 }
+
+
+[1, 2, 3, 4, 5].filter(function (num) { return num > 3; });
